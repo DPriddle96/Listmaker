145e00b5b1aad77c448914be305b1c1dd853881c
Dan Priddle <daniel.r.priddle@gmail.com>
2022-09-08 11:00:28 -0400

    end of part 2

--- app/src/main/java/com/danielpriddle/listmaker/TodoListAdapter.kt
+++ app/src/main/java/com/danielpriddle/listmaker/TodoListAdapter.kt
@@ -5,9 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.danielpriddle.listmaker.databinding.TodoListViewHolderBinding
 
-class TodoListAdapter : RecyclerView.Adapter<TodoListViewHolder>() {
-
-    private val todoLists = arrayOf("Android Development", "House Work", "Errands", "Shopping")
+class TodoListAdapter(val lists: ArrayList<TaskList>) : RecyclerView.Adapter<TodoListViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoListViewHolder {
         val binding = TodoListViewHolderBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -16,10 +14,16 @@ class TodoListAdapter : RecyclerView.Adapter<TodoListViewHolder>() {
 
     override fun onBindViewHolder(holder: TodoListViewHolder, position: Int) {
         holder.listPositionTextView.text = (position + 1).toString()
-        holder.listTitleTextView.text = todoLists[position]
+        holder.listTitleTextView.text = lists[position].name
     }
 
     override fun getItemCount(): Int {
-        return todoLists.size
+        return lists.size
+    }
+
+
+    fun addList(list: TaskList) {
+        lists.add(list)
+        notifyItemInserted(lists.size - 1)
     }
 }
